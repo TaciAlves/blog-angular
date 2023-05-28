906137ed27232b54a95649c722ffe88e8598290a
TaciAlves <taciara.edla@outlook.com>
2023-05-28 16:10:37 -0300

    finished and responsive project

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,32 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "photoCover": "https://static1.purebreak.com.br/articles/2/58/07/2/@/237523-robert-downey-jr-conta-que-deixara-de-i-amp_article_image-2.jpg",
+        "title": "O RETORNO DA LENDA",
+        "description": "Marvel anuncia o retorno de Robert Downey Jr como Homem de Ferro no novo filme do herói."
+    },
+    {
+        "id": "2",
+        "photoCover": "https://www.ofuxico.com.br/wp-content/uploads/2022/01/eterns-reunidos-em-centro-de-poster.jpg",
+        "title": "ETERNOS GANHARÁ SÉRIE",
+        "description": "Marvel anuncia que o longe Eternos ganhará sequencia em forma de série no Disney +, contando a história de cada um dos heróis."
+    },
+    {
+        "id": "3",
+        "photoCover": "https://img.elo7.com.br/product/original/37EBEF7/painel-1x65cm-pantera-negra-wakanda-festa-expressa.jpg",
+        "title": "Marvel anuncia nova série em homenagem ao Pantera Negra",
+        "description": "Marvel anuncia nova série em homenagem ao Pantera Negra, seu lançamento será ainda em 2023."
+    },
+    {
+        "id": "4",
+        "photoCover": "https://portalpopline.com.br/wp-content/uploads/2022/11/thor-capa.jpg",
+        "title": "Thor: Amor e Trovão terá uma continuação afirma diretor",
+        "description": "Thor: Amor e Trovão terá uma continuação afirma diretor, o longa terá foco na vida do herói como pai."
+    },
+    {
+        "id": "5",
+        "photoCover": "https://sm.ign.com/ign_br/screenshot/default/an-loki-disney_mpp5.jpg",
+        "title": "Segunda temporada de Loki chegará em breve ao Disney +",
+        "description": "Segunda temporada de Loki chegará em breve ao Disney +, afirma diretor."
+    },
+]
